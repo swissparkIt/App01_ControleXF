9c46341c29dca966b17d354d131f44170243ccd4
julio <julio@DESKTOP-HEHU36U>
2019-03-10 23:38:27 -0300

    Adicionar arquivos de projeto.

---
+++ App01_ControleXF/App01_ControleXF/Controles/ProgressBarPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_ControleXF.Controles
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ProgressBarPage : ContentPage
+	{
+		public ProgressBarPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void btnModificar_Click(object sender, EventArgs args)
+        {
+            pb1.Progress = 1;
+            pb2.ProgressTo(1, 5000, Easing.Linear);
+            pb3.ProgressTo(1, 5000, Easing.SpringIn);
+        }
+
+
+    }
+}
---
+++ App01_ControleXF/App01_ControleXF/Master/Master.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_ControleXF.Master
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Master : MasterDetailPage
+	{
+		public Master ()
+		{
+			InitializeComponent ();
+		}
+
+        private void btnActivityIndicator_Click(object sender, EventArgs args)
+        {
+            Detail = new App01_ControleXF.Controles.ActivityIndicatorPage();
+            IsPresented = false;
+        }
+
+        private void btnProgressBar_Click(object sender, EventArgs args)
+        {
+            Detail = new App01_ControleXF.Controles.ProgressBarPage();
+            IsPresented = false;
+        }
+        
+
+    }
+}
