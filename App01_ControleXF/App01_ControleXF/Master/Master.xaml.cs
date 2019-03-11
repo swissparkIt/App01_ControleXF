1f1593f21ff2d404e521c619f8952360368efcc6
julio <juliodisouza@gmail.com>
2019-03-11 13:14:26 -0300

    swiss - aulas

--- App01_ControleXF/App01_ControleXF/Master/Master.xaml.cs
+++ App01_ControleXF/App01_ControleXF/Master/Master.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using App01_ControleXF.Controles;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -19,7 +20,7 @@ namespace App01_ControleXF.Master
 
         private void btnActivityIndicator_Click(object sender, EventArgs args)
         {
-            Detail = new App01_ControleXF.Controles.ActivityIndicatorPage();
+            Detail = new ActivityIndicatorPage();
             IsPresented = false;
         }
 
@@ -28,7 +29,30 @@ namespace App01_ControleXF.Master
             Detail = new App01_ControleXF.Controles.ProgressBarPage();
             IsPresented = false;
         }
-        
 
+        private void btnBoxViewPage_Click(object sender, EventArgs args)
+        {
+            Detail = new App01_ControleXF.Controles.BoxViewPage();
+            IsPresented = false;
+        }
+
+        private void btnLabelPage_Click(object sender, EventArgs args)
+        {
+            Detail = new App01_ControleXF.Controles.LabelPage();
+            IsPresented = false;
+        }
+
+        private void btnButtonPage_Click(object sender, EventArgs args)
+        {
+            Detail = new ButtonPage();
+            IsPresented = false;
+        }
+
+        private void btnEntryEditorPage_Click(object sender, EventArgs args)
+        {
+            Detail = new EntryEditorPage();
+            IsPresented = false;
+        }
+        
     }
 }
