701faf6ed81db97565016cdc12e0e1b32eac0764
julio <juliodisouza@gmail.com>
2019-03-19 13:08:36 -0300

    swiss: aulas - tableview e webview

---
+++ App01_ControleXF/App01_ControleXF/Controles/WebViewPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_ControleXF.Controles
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class WebViewPage : ContentPage
+	{
+
+		public WebViewPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void btnIr_Click(object sender, EventArgs e)
+        {
+            Navegador.Source = txtEndereco.Text;
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            if (Navegador.CanGoBack)
+            {
+                Navegador.GoBack();
+            }
+        }
+
+        private void btnProximo_Click(object sender, EventArgs e)
+        {
+            if (Navegador.CanGoForward)
+            {
+                Navegador.GoForward();
+            }
+        }
+
+        private void Navigating_Event(object sender, WebNavigatingEventArgs e)
+        {
+            lblStatus.Text = "Carregando...";
+        }
+
+        private void Navigated_Event(object sender, WebNavigatedEventArgs e)
+        {
+            lblStatus.Text = "Pronto.";
+        }
+
+    }
+}
--- App01_ControleXF/App01_ControleXF/Master/Master.xaml.cs
+++ App01_ControleXF/App01_ControleXF/Master/Master.xaml.cs
@@ -13,6 +13,7 @@ namespace App01_ControleXF.Master
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Master : MasterDetailPage
 	{
+
 		public Master ()
 		{
 			InitializeComponent ();
@@ -20,86 +21,99 @@ namespace App01_ControleXF.Master
 
         private void btnActivityIndicator_Click(object sender, EventArgs args)
         {
-            Detail = new ActivityIndicatorPage();
+            Detail = new NavigationPage(new ActivityIndicatorPage());
             IsPresented = false;
         }
 
         private void btnProgressBar_Click(object sender, EventArgs args)
         {
-            Detail = new App01_ControleXF.Controles.ProgressBarPage();
+            Detail = new NavigationPage(new App01_ControleXF.Controles.ProgressBarPage());
             IsPresented = false;
         }
 
         private void btnBoxViewPage_Click(object sender, EventArgs args)
         {
-            Detail = new App01_ControleXF.Controles.BoxViewPage();
+            Detail = new NavigationPage(new App01_ControleXF.Controles.BoxViewPage());
             IsPresented = false;
         }
 
         private void btnLabelPage_Click(object sender, EventArgs args)
         {
-            Detail = new App01_ControleXF.Controles.LabelPage();
+            Detail = new NavigationPage(new App01_ControleXF.Controles.LabelPage());
             IsPresented = false;
         }
 
         private void btnButtonPage_Click(object sender, EventArgs args)
         {
-            Detail = new ButtonPage();
+            Detail = new NavigationPage(new ButtonPage());
             IsPresented = false;
         }
 
         private void btnEntryEditorPage_Click(object sender, EventArgs args)
         {
-            Detail = new EntryEditorPage();
+            Detail = new NavigationPage(new EntryEditorPage());
             IsPresented = false;
         }
 
         private void btnDatePickerPage_Click(object sender, EventArgs args)
         {
-            Detail = new DatePickerPage();
+            Detail = new NavigationPage(new DatePickerPage());
             IsPresented = false;
         }
 
         private void btnTimePickerPage_Click(object sender, EventArgs args)
         {
-            Detail = new TimePickerPage();
+            Detail = new NavigationPage(new TimePickerPage());
             IsPresented = false;
         }
 
         private void btnPickerPage_Click(object sender, EventArgs e)
         {
-            Detail = new PickerPage();
+            Detail = new NavigationPage(new PickerPage());
             IsPresented = false;
         }
 
         private void btnSearchBarPage_Click(object sender, EventArgs e)
         {
-            Detail = new SearchBarPage();
+            Detail = new NavigationPage(new SearchBarPage());
             IsPresented = false;
         }
 
         private void SliderStepperPage_Click(object sender, EventArgs e)
         {
-            Detail = new SliderStepperPage();
+            Detail = new NavigationPage(new SliderStepperPage());
             IsPresented = false;
         }
 
         private void btnSwitchPage_Click(object sender, EventArgs e)
         {
-            Detail = new SwitchPage();
+            Detail = new NavigationPage(new SwitchPage());
             IsPresented = false;
         }
 
         private void btnImagePage_Click(object sender, EventArgs e)
         {
-            Detail = new ImagePage();
+            Detail = new NavigationPage(new ImagePage());
             IsPresented = false;
         }
 
         private void btnListViewPage_Click(object sender, EventArgs e)
         {
-            Detail = new ListViewPage();
+            Detail = new NavigationPage(new ListViewPage());
             IsPresented = false;
         }
+
+        private void btnTableViewPage_Click(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new TableViewPage());
+            IsPresented = false;
+        }
+
+        private void btnWebViewPage_Click(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new WebViewPage());
+            IsPresented = false;
+        }
+
     }
 }
